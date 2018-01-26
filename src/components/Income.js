b81af88c32a813e8faf9cc9c9bbe5a8a916a42a1
treemmett <treemmett@gmail.com>
2018-01-25 20:09:32 -0700

    Adds currency formatter

--- src/components/Income.js
+++ src/components/Income.js
@@ -6,6 +6,37 @@ export default class Income extends Component{
     alert('Done');
   }
 
+  formatCurrency = e => {
+    //Remove all non currency characters from value
+    const val = e.target.value.trim().replace(/[^\d.{1}]/g, '');
+
+    //Remove all periods after the first
+    const split = val.split('.');
+
+    //Seperate dollars and cents
+    const dollars = split.shift();
+    let cents = split.join('').substring(0,2);
+
+    //Add decimal if needed
+    if(split.length || e.type === 'blur'){
+      cents = '.' + cents;
+    }
+
+    //Add padding to cents
+    if(e.type === 'blur'){
+      cents += '0'.repeat(3 - cents.length);
+    }
+
+    //Store numeric value
+    const value = Number(dollars + cents);
+
+    if(e.type === 'blur' && value === 0){
+      e.target.value = null;
+    }else{
+      e.target.value = '$' + dollars.replace(/\B(?=(\d{3})+(?!\d))/g, ",") + cents;
+    }
+  }
+
   render(){
     return (
       <div className="app">
@@ -29,7 +60,7 @@ export default class Income extends Component{
             <input id="income_hours" name="income_hours" required/>
 
             <label htmlFor="income_rate">Income Rate</label>
-            <input id="income_rate" name="income_rate" placeholder="$00.00" required/>
+            <input id="income_rate" name="income_rate" placeholder="$00.00" onChange={this.formatCurrency} onFocus={this.formatCurrency} onBlur={this.formatCurrency}required/>
 
             <input type="submit" value="Save"/>
           </form>
