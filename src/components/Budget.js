78e2358c1810650144c3f70e1c64700de807087c
treemmett <treemmett@gmail.com>
2018-02-04 19:13:46 -0700

    Adds budget category creation

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Plus from '../svg/Plus';
 import './Budget.scss';
 
 export default class Budget extends Component{
@@ -19,15 +20,51 @@ export default class Budget extends Component{
     fetch('/api/budget').then(res => res.json()).then(res => this.setState({categories: res}));
   }
 
+  createCategory = (e, that) => {
+    const saveCategory = e1 => {
+      e1.preventDefault();
+
+      //Duplicate state
+      const categories = this.state.categories.slice(0);
+
+      //Find category with ID
+      for(let i = 0; i < categories.length; i++){
+        if(categories[i].id !== that.props.id){
+          continue;
+        }
+
+        //Add new category to duplicate
+        categories[i].subcategories.push({name: e1.target.name.value.trim(), spending: 0});
+
+        //Update state
+        this.setState({categories: categories, modal: null});
+      }
+    }
+
+    //Create modal for new category
+    const modal = <div className="modal">
+      <form onSubmit={saveCategory}>
+        <label htmlFor="category_name">Category Name</label>
+        <input id="category_name" name="name" autoFocus required/>
+        <input type="submit" value="Save"/>
+        <button onClick={_ => this.setState({modal: null})} className="secondary">Cancel</button>
+      </form>
+    </div>
+
+    //Add modal to DOM
+    this.setState({modal: modal});
+  }
+
   render(){
     //Display each category
     let categories = [];
     this.state.categories.forEach((i, index) => {
-      categories.push(<Category name={i.name} categories={i.subcategories} id={i.id} key={index}/>)
+      categories.push(<Category createCategory={this.createCategory} name={i.name} categories={i.subcategories} id={i.id} key={index}/>)
     });
 
     return (
       <div className="app budget">
+        { this.state.modal }
         <div className="table">
           {categories}
         </div>
@@ -54,6 +91,7 @@ class Category extends Component{
       <div className="category">
         <div className="main">
           <div className="name">{this.props.name}</div>
+          <Plus className="add" onClick={e => this.props.createCategory(e, this)}/>
           <div className="spending">{totalSpending}</div>
         </div>
         {rows}
