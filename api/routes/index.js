c09e4f9824ac3c0b7603894ae4406278fea37551
treemmett <tregan@tregan.me>
2018-11-15 21:13:45 -0700

    Switches to Vue

--- api/routes/index.js
+++
@@ -1,7 +0,0 @@
-const routes = require('express').Router();
-
-routes.use('/auth', require('./auth'));
-routes.use('/budget', require('./budget'));
-routes.use('/taxable', require('./taxable'));
-
-module.exports = routes;
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const autoprefixer = require('autoprefixer');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'main.[hash:6].js'
+  },
+
+  resolve: {
+    extensions: ['.js', '.json', '.vue'],
+    modules: [
+      path.join(__dirname, 'node_modules')
+    ],
+    alias: {
+      vue: 'vue/dist/vue.js'
+    }
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [
+                autoprefixer()
+              ]
+            }
+          },
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(['build']),
+
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: require('html-webpack-template'),
+
+      appMountId: 'app',
+      mobile: true,
+      lang: 'en-US'
+    }),
+
+    new VueLoaderPlugin()
+  ],
+
+  devServer: {
+    historyApiFallback: true,
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  }
+}
