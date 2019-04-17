b01158ddca4e936f58d48a7421720dd3eb587f98
treemmett <tregan@tregan.me>
2019-04-16 22:49:45 -0600

    feat: add login view

---
+++ src/components/Input.vue
@@ -0,0 +1,72 @@
+<template>
+  <label class="wrapper">
+    <div class="main">
+      <div class="label">Email</div>
+      <input class="input" />
+    </div>
+  </label>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Input extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+@import '../global';
+
+$fontSize: 16px;
+
+.wrapper {
+  display: block;
+  padding-top: 5px;
+}
+
+.main {
+  position: relative;
+  border-radius: 4px;
+  border: 1px solid #ccc;
+  transition: border-color 0.2s $curve, box-shadow 0.2s $curve;
+}
+
+.label {
+  position: absolute;
+  bottom: 0.8em;
+  left: calc(0.9em - 5px);
+  font-size: 16px;
+  font-size: $fontSize;
+  color: #777;
+  background-color: rgba(#fff, 0);
+  padding: 0 5px;
+  pointer-events: none;
+  transition: 0.2s $curve;
+  transition-property: background-color, color, transform;
+}
+
+.input {
+  font-size: $fontSize;
+  border: 0;
+  padding: 0.8em 0.9em;
+  width: 100%;
+  box-sizing: border-box;
+  outline: none;
+  font-family: $font-stack;
+  border-radius: 4px;
+}
+
+.wrapper:focus-within,
+.wrapper.active {
+  .main {
+    box-shadow: 0 0 3px 0 $blue;
+    border-color: $blue;
+  }
+
+  .label {
+    transform: translateX(-1em) translateY(-1.5em) scale(0.8);
+    color: darken($blue, 5%);
+    background-color: #fff;
+  }
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="view">
+    <div class="card">
+      <div class="half">
+        <div class="brand">Rudget</div>
+      </div>
+      <div class="half">
+        <Input />
+        <Input />
+        <Input />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import Input from '@/components/Input.vue';
+
+@Component({
+  components: {
+    Input
+  }
+})
+export default class Login extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.view {
+  padding: 5em;
+}
+
+.card {
+  display: flex;
+  position: relative;
+  background-color: #fff;
+  width: 100%;
+  box-sizing: border-box;
+  border-radius: 5px;
+  overflow: hidden;
+}
+
+.half {
+  // float: left;
+  position: relative;
+  // height: 100%;
+  padding: 2em 1em;
+  width: 50%;
+  box-sizing: border-box;
+
+  &:first-child {
+    background-color: #297c45;
+  }
+}
+
+.brand {
+  color: #fff;
+  font-size: 38px;
+  font-weight: 500;
+  text-align: center;
+}
+</style>
